b77c1fedd5602515978b044c6574f6ac98affbe8
MuhammedAnasEP <anasep1110@gmail.com>
2024-06-06 10:50:25 +0530

    create serializer and view for Register the user

---
+++ backend/authe/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'password': {'write_only': True},
+            'password2': {'write_only': True}
+        }
+
+    def save(self):
+        """
+        Save the user registration data to the database.
+        This method creates a new user instance based on the validated data provided.
+        It checks if a user with the same email already exists in the database. 
+        If it does, a `ValidationError` is raised with the message "A user with that email already exists."
+        If the passwords do not match, a `ValidationError` is raised with the message "Passwords do not match!".
+        """
+        user = User(
+            first_name=self.validated_data['first_name'],
+            last_name=self.validated_data['last_name'],
+            username=self.validated_data['username'],
+        )
+
+        email=self.validated_data['email']
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if User.objects.filter(email=email):
+            raise serializers.ValidationError(
+                {'email': 'A user with that email already exists.'})
+
+        if password != password2:
+            raise serializers.ValidationError(
+                {'password': 'Passwords do not match!'})
+
+        user.email = email
+        user.set_password(password)
+        user.save()
+
+        return user
--- backend/authe/views.py
+++ backend/authe/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
 # Create your views here.
+
+class Register(APIView):
+    def post(setlf, request):
+        """
+        Handles the POST request for user registration.
+        First check the data using serializer. If the data is valid, save the user and return the response.
+        If the data is not valid, return the error message.
+        """
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            if user is not None:
+                return Response({'status':'Registered', 'data':serializer.data}, status=status.HTTP_201_CREATED)
+        
+        return Response({'error':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
